99df4a9f44647d5d7c1cc3ed5725e0453299e1fc
harshareddy979 <dharshavardhanrrr@gmail.com>
2024-08-07 19:27:05 +0530

    added object iteration

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -8,9 +8,12 @@ import "./Homepage.css"
 function HomePage(props){
     // const userName=useContext(UserContext);
     // const valuesLogin=useContext(LoginContext)
-    const [fruitsArray,setFruitsArray]=
-    useState(["apple","banana","pineapple","grapes","mango"])
+    const [fruitsArray,setFruitsArray]=useState(["apple","banana","pineapple","grapes","mango"])
     const [fruitName,setFruitName]=useState("")
+    const [fruitsObject,setFruitsObject]=useState({name:"apple",color:"red"})
+    const [key,setKey]=useState("")
+    const[value,setValue]=useState("")
+    const [gender,setGender]=useState("")
 
     const navigate=useNavigate()
     return(
@@ -20,6 +23,16 @@ function HomePage(props){
             {"username of login page is "+valuesLogin?.userName}
             {"password is "+valuesLogin?.password} */}
             <h1>Welcome To Homepage {props.loginUserName}</h1>
+            <h2>Gender</h2>
+            <input type="radio" checked={gender==="male"} onClick={()=>{setGender("male")}}></input>
+            <label>Male</label>
+            <input type="radio" checked={gender==="female"} onClick={()=>{setGender("female")}}></input>
+            <label>Female</label>
+            <br></br>
+            {gender !== "" &&
+            <div>
+            {gender === "male" &&
+            <div>   
             <input type="text" value={fruitName} onChange={(e)=>{setFruitName(e.target.value)}}></input>
             <button onClick={()=>{fruitName && setFruitsArray([...fruitsArray, fruitName]);
             setFruitName("")
@@ -31,13 +44,33 @@ function HomePage(props){
                 return <div className="homepage">{ele}</div>
             })}
             </div>
+            </div>
+            }
             <br/>
+            {gender === "female" &&
+            <div>
+            <label>key</label>
+            <input type="text" value={key} onChange={(e)=>{setKey(e.target.value)}}></input>
+            <label>value</label>
+            <input type="text" value={value} onChange={(e)=>{setValue(e.target.value)}}></input>
+            <button onClick={()=>{setFruitsObject({...fruitsObject,key:value})}}>Add Object</button>
+            <h2>Fruits Object</h2>
+            <div className="homepage2">
+            {Object.entries(fruitsObject).map(([key,val],index)=>{
+                return <div className="homepage">{key+" : "+val}</div>
+            })}
+            </div>
+            </div>
+            }
+            </div>
+          } 
             <button onClick={()=>{
                 navigate("/")
                 // props.setLoginFlag(false);props.setUserName("");
                 // props.setPassword("");
                 // props.obj({})
             }}>Logout</button>
+
         </div>
     )
 }
