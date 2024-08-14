da3aaa64bf7b398094bdc5da0bb8415e940a72cc
harshareddy979 <dharshavardhanrrr@gmail.com>
2024-08-14 19:28:52 +0530

    added api calls

--- src/Components/apiCalls.js
+++ src/Components/apiCalls.js
@@ -12,6 +12,34 @@ function ApiCalls(){
         console.log(response);   
     }
 
+    async function postData(){
+        var apiData={
+            "name":"besant",
+            "age":"100",
+            "title":"data created successfully"
+        }
+        var response=await axios.post("https://jsonplaceholder.typicode.com/posts",apiData)
+        console.log(response);
+        setData(response.data)  
+    }
+
+    async function putData(){
+        var apiData={
+            "userId": 777,
+            "title": "updated successfully",
+            "body": "new body added"
+        }
+        var response=await axios.put("https://jsonplaceholder.typicode.com/posts/1",apiData)
+        console.log(response);
+        setData(response.data)
+    }
+
+    async function deleteData(){
+        var response=await axios.delete("https://jsonplaceholder.typicode.com/posts/1")
+        console.log(response);
+        setData(response.data)
+    }
+
     // useEffect(()=>{
     //     getData()
     // },[])
@@ -20,9 +48,14 @@ function ApiCalls(){
     return(
         <div>
             <h1>Api Calls</h1>
-            {data && data.title}
+            {data && data.title
+            // data.map((e,i)=>{return e.id})
+            }
             <div>
                 <button onClick={()=>{getData()}}>getData</button>
+                <button onClick={()=>{postData()}}>postData</button>
+                <button onClick={()=>{putData()}}>putData</button>
+                <button onClick={()=>{deleteData()}}>deleteData</button>
             </div>
         </div>
     )
